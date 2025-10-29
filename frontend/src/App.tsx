2b799993523c1ec2e02b299cc391357e44781191
Jamal Ahmed <43161576+Jman1868@users.noreply.github.com>
2025-10-28 21:44:43 -0700

    remove unused Link import from App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Link } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import StudentPage from './routes/StudentPage'
 import InstructorPage from './routes/InstructorPage'
 import Homepage from './routes/Homepage'
