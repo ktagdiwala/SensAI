faf6c71f1f4341d7bd30030ce3a281669916bf75
Jamal Ahmed <43161576+Jman1868@users.noreply.github.com>
2025-10-28 21:36:44 -0700

    created navbar component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,21 +3,17 @@ import StudentPage from './routes/StudentPage'
 import InstructorPage from './routes/InstructorPage'
 import Homepage from './routes/Homepage'
 
+import Nav from './components/nav'
+
 function App() {
   return (
     <>
-      <nav className='flex'>
-        <Link to="/students" className='flex-auto'>Students</Link>
-        <Link to="/instructors" className='flex-auto'>Instructors</Link>
-      </nav>
-
+      <Nav />
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/students" element={<StudentPage />} />
         <Route path="/instructors" element={<InstructorPage />} />
       </Routes>
-
-
     </>
   )
 }
