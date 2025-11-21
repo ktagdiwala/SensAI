360c47ea4dda702b7fb7c2f1fca96b96837c69bf
Jamal Ahmed <43161576+Jman1868@users.noreply.github.com>
2025-11-20 18:59:25 -0800

    Update CORS middleware to remove OPTIONS method from allowed methods

--- backend/app.js
+++ backend/app.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
 	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
 	res.header('Access-Control-Allow-Credentials', 'true');
 	if (req.method === 'OPTIONS') {
-		res.header('Access-Control-Allow-Methods', 'POST, GET,  PUT, DELETE, OPTIONS');
+		res.header('Access-Control-Allow-Methods', 'POST, GET,  PUT, DELETE');
 		return res.status(200).json({});
 	}
 	next();
